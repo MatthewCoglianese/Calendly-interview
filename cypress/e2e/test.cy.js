b4e6259d146ecc5d4c79d1826e7d78c20800196e
Matt Coglianese <mcoglianese@springbot.com>
2024-11-15 11:51:18 -0500

    Interview
    Interview

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,24 @@
+let numberEntered = 3
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
+  beforeEach((), =>{
+    cy.visit(/)
+    cy.get('#number')
+    .type(numberEntered)
+    cy,get('#start')
+    .click()
   })
+  it('Can type in number field', () => {
+    cy.visit('/')
+    cy.get('#number')
+    .type(numberEntered)
+    .should('have.text', numberEntered)
+  
+  })
+  it('Can click play button', () => {
+    cy.get('#'((numberEntered*numberEntered)-1)
+    .should('exist')
+  })
+
 })
+
+  
